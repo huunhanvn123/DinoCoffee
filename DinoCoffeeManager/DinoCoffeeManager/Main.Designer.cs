d6358995238073a2c8acf0ec93e87941e665bc8c
Thanh Nhan <thanhnhanht219@gmail.com>
2020-06-25 09:54:55 +0700

    Create Winform app

---
+++ DinoCoffeeManager/DinoCoffeeManager/Main.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace DinoCoffeeManager
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.pnTab = new System.Windows.Forms.Panel();
+            this.pnSide = new System.Windows.Forms.Panel();
+            this.btnBaoCao = new System.Windows.Forms.Button();
+            this.btnKhachHang = new System.Windows.Forms.Button();
+            this.btnQuy = new System.Windows.Forms.Button();
+            this.btnQuanLy = new System.Windows.Forms.Button();
+            this.btnPOS = new System.Windows.Forms.Button();
+            this.btnShowHideTab = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lbTest = new System.Windows.Forms.Label();
+            this.pnTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnShowHideTab)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnTab
+            // 
+            this.pnTab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(255)))), ((int)(((byte)(153)))));
+            this.pnTab.Controls.Add(this.pnSide);
+            this.pnTab.Controls.Add(this.btnBaoCao);
+            this.pnTab.Controls.Add(this.btnKhachHang);
+            this.pnTab.Controls.Add(this.btnQuy);
+            this.pnTab.Controls.Add(this.btnQuanLy);
+            this.pnTab.Controls.Add(this.btnPOS);
+            this.pnTab.Controls.Add(this.btnShowHideTab);
+            this.pnTab.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnTab.Location = new System.Drawing.Point(0, 0);
+            this.pnTab.Name = "pnTab";
+            this.pnTab.Size = new System.Drawing.Size(250, 676);
+            this.pnTab.TabIndex = 0;
+            // 
+            // pnSide
+            // 
+            this.pnSide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.pnSide.Location = new System.Drawing.Point(0, 88);
+            this.pnSide.Name = "pnSide";
+            this.pnSide.Size = new System.Drawing.Size(11, 58);
+            this.pnSide.TabIndex = 1;
+            // 
+            // btnBaoCao
+            // 
+            this.btnBaoCao.FlatAppearance.BorderSize = 0;
+            this.btnBaoCao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBaoCao.Font = new System.Drawing.Font("#9Slide03 Bebas Neue ZSmall", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBaoCao.Image = ((System.Drawing.Image)(resources.GetObject("btnBaoCao.Image")));
+            this.btnBaoCao.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBaoCao.Location = new System.Drawing.Point(12, 314);
+            this.btnBaoCao.Name = "btnBaoCao";
+            this.btnBaoCao.Size = new System.Drawing.Size(238, 58);
+            this.btnBaoCao.TabIndex = 2;
+            this.btnBaoCao.Text = "     BÁO CÁO";
+            this.btnBaoCao.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnBaoCao.UseVisualStyleBackColor = true;
+            this.btnBaoCao.Click += new System.EventHandler(this.btnBaoCao_Click);
+            // 
+            // btnKhachHang
+            // 
+            this.btnKhachHang.FlatAppearance.BorderSize = 0;
+            this.btnKhachHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKhachHang.Font = new System.Drawing.Font("#9Slide03 Bebas Neue ZSmall", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnKhachHang.Image = ((System.Drawing.Image)(resources.GetObject("btnKhachHang.Image")));
+            this.btnKhachHang.Location = new System.Drawing.Point(12, 200);
+            this.btnKhachHang.Name = "btnKhachHang";
+            this.btnKhachHang.Size = new System.Drawing.Size(238, 58);
+            this.btnKhachHang.TabIndex = 2;
+            this.btnKhachHang.Text = "     KHÁCH HÀNG";
+            this.btnKhachHang.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnKhachHang.UseVisualStyleBackColor = true;
+            this.btnKhachHang.Click += new System.EventHandler(this.btnKhachHang_Click);
+            // 
+            // btnQuy
+            // 
+            this.btnQuy.FlatAppearance.BorderSize = 0;
+            this.btnQuy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuy.Font = new System.Drawing.Font("#9Slide03 Bebas Neue ZSmall", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnQuy.Image = ((System.Drawing.Image)(resources.GetObject("btnQuy.Image")));
+            this.btnQuy.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnQuy.Location = new System.Drawing.Point(12, 257);
+            this.btnQuy.Name = "btnQuy";
+            this.btnQuy.Size = new System.Drawing.Size(238, 58);
+            this.btnQuy.TabIndex = 2;
+            this.btnQuy.Text = "     QUỸ";
+            this.btnQuy.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnQuy.UseVisualStyleBackColor = true;
+            this.btnQuy.Click += new System.EventHandler(this.btnQuy_Click);
+            // 
+            // btnQuanLy
+            // 
+            this.btnQuanLy.FlatAppearance.BorderSize = 0;
+            this.btnQuanLy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuanLy.Font = new System.Drawing.Font("#9Slide03 Bebas Neue ZSmall", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnQuanLy.Image = ((System.Drawing.Image)(resources.GetObject("btnQuanLy.Image")));
+            this.btnQuanLy.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnQuanLy.Location = new System.Drawing.Point(12, 145);
+            this.btnQuanLy.Name = "btnQuanLy";
+            this.btnQuanLy.Size = new System.Drawing.Size(238, 58);
+            this.btnQuanLy.TabIndex = 2;
+            this.btnQuanLy.Text = "     QUẢN LÝ";
+            this.btnQuanLy.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnQuanLy.UseVisualStyleBackColor = true;
+            this.btnQuanLy.Click += new System.EventHandler(this.btnQuanLy_Click);
+            // 
+            // btnPOS
+            // 
+            this.btnPOS.FlatAppearance.BorderSize = 0;
+            this.btnPOS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPOS.Font = new System.Drawing.Font("#9Slide03 Bebas Neue ZSmall", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPOS.Image = ((System.Drawing.Image)(resources.GetObject("btnPOS.Image")));
+            this.btnPOS.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnPOS.Location = new System.Drawing.Point(12, 88);
+            this.btnPOS.Name = "btnPOS";
+            this.btnPOS.Size = new System.Drawing.Size(238, 58);
+            this.btnPOS.TabIndex = 2;
+            this.btnPOS.Text = "     POS";
+            this.btnPOS.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPOS.UseVisualStyleBackColor = true;
+            this.btnPOS.Click += new System.EventHandler(this.btnPOS_Click);
+            // 
+            // btnShowHideTab
+            // 
+            this.btnShowHideTab.Image = ((System.Drawing.Image)(resources.GetObject("btnShowHideTab.Image")));
+            this.btnShowHideTab.Location = new System.Drawing.Point(0, 0);
+            this.btnShowHideTab.Margin = new System.Windows.Forms.Padding(2);
+            this.btnShowHideTab.Name = "btnShowHideTab";
+            this.btnShowHideTab.Size = new System.Drawing.Size(230, 83);
+            this.btnShowHideTab.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnShowHideTab.TabIndex = 1;
+            this.btnShowHideTab.TabStop = false;
+            this.btnShowHideTab.Click += new System.EventHandler(this.btnShowHideTab_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(250, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1012, 82);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.panel3.Controls.Add(this.lbTest);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(250, 82);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1012, 594);
+            this.panel3.TabIndex = 2;
+            // 
+            // lbTest
+            // 
+            this.lbTest.AutoSize = true;
+            this.lbTest.Font = new System.Drawing.Font("#9Slide03 Bebas Neue ZSmall", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbTest.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbTest.Location = new System.Drawing.Point(462, 194);
+            this.lbTest.Name = "lbTest";
+            this.lbTest.Size = new System.Drawing.Size(192, 126);
+            this.lbTest.TabIndex = 1;
+            this.lbTest.Text = "POS";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(1262, 676);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnTab);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Main";
+            this.Text = "Main";
+            this.pnTab.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnShowHideTab)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnTab;
+        private System.Windows.Forms.PictureBox btnShowHideTab;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnPOS;
+        private System.Windows.Forms.Button btnQuy;
+        private System.Windows.Forms.Button btnQuanLy;
+        private System.Windows.Forms.Panel pnSide;
+        private System.Windows.Forms.Button btnBaoCao;
+        private System.Windows.Forms.Button btnKhachHang;
+        private System.Windows.Forms.Label lbTest;
+    }
+}
---
+++ DinoCoffeeManager/DinoCoffeeManager/Main.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DinoCoffeeManager
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            pnSide.Height = btnPOS.Height;
+            pnSide.Top = btnPOS.Top;
+            lbTest.Text = "POS";
+            btnPOS.BackColor = Color.FromArgb(242, 242, 242);
+        }
+
+        private void btnShowHideTab_Click(object sender, EventArgs e)
+        {
+            if(pnTab.Width == 250)
+            {
+                pnTab.Width = 91;
+            }
+            else
+            {
+                pnTab.Width = 250;
+            }
+        }
+
+        private void btnPOS_Click(object sender, EventArgs e)
+        {
+            pnSide.Height = btnPOS.Height;
+            pnSide.Top = btnPOS.Top;
+            btnPOS.BackColor = Color.FromArgb(242, 242, 242);
+            btnQuanLy.BackColor = Color.FromArgb(204, 255, 153);
+            btnKhachHang.BackColor = Color.FromArgb(204, 255, 153);
+            btnQuy.BackColor = Color.FromArgb(204, 255, 153);
+            btnBaoCao.BackColor = Color.FromArgb(204, 255, 153);
+            lbTest.Text = "POS";
+        }
+
+        private void btnQuanLy_Click(object sender, EventArgs e)
+        {
+            pnSide.Height = btnQuanLy.Height;
+            pnSide.Top = btnQuanLy.Top;
+            btnQuanLy.BackColor = Color.FromArgb(242, 242, 242);
+            btnPOS.BackColor = Color.FromArgb(204, 255, 153);
+            btnKhachHang.BackColor = Color.FromArgb(204, 255, 153);
+            btnQuy.BackColor = Color.FromArgb(204, 255, 153);
+            btnBaoCao.BackColor = Color.FromArgb(204, 255, 153);
+            lbTest.Text = "QuanLy";
+        }
+
+        private void btnKhachHang_Click(object sender, EventArgs e)
+        {
+            pnSide.Height = btnKhachHang.Height;
+            pnSide.Top = btnKhachHang.Top;
+            btnKhachHang.BackColor = Color.FromArgb(242, 242, 242);
+            btnPOS.BackColor = Color.FromArgb(204, 255, 153);
+            btnQuanLy.BackColor = Color.FromArgb(204, 255, 153);
+            btnQuy.BackColor = Color.FromArgb(204, 255, 153);
+            btnBaoCao.BackColor = Color.FromArgb(204, 255, 153);
+            lbTest.Text = "Khách hàng";
+        }
+
+        private void btnQuy_Click(object sender, EventArgs e)
+        {
+            pnSide.Height = btnQuy.Height;
+            pnSide.Top = btnQuy.Top;
+            btnQuy.BackColor = Color.FromArgb(242, 242, 242);
+            btnPOS.BackColor = Color.FromArgb(204, 255, 153);
+            btnKhachHang.BackColor = Color.FromArgb(204, 255, 153);
+            btnQuanLy.BackColor = Color.FromArgb(204, 255, 153);
+            btnBaoCao.BackColor = Color.FromArgb(204, 255, 153);
+            lbTest.Text = "Qũy";
+        }
+
+        private void btnBaoCao_Click(object sender, EventArgs e)
+        {
+            pnSide.Height = btnBaoCao.Height;
+            pnSide.Top = btnBaoCao.Top;
+            btnBaoCao.BackColor = Color.FromArgb(242, 242, 242);
+            btnPOS.BackColor = Color.FromArgb(204, 255, 153);
+            btnKhachHang.BackColor = Color.FromArgb(204, 255, 153);
+            btnQuy.BackColor = Color.FromArgb(204, 255, 153);
+            btnQuanLy.BackColor = Color.FromArgb(204, 255, 153);
+            lbTest.Text = "Báo cáo";
+        }
+    }
+}
